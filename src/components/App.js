29cd8562eb0e2748cbdf25996f5bb06a2d80d1d5
Mario <mariob360@gmail.com>
2019-09-05 19:17:04 -0700

    Alter folder structure, Create Palette component

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Palette from './Palette';
+import seedColors from '../assets/seedColors';
+
+function App() {
+  return (
+    <div className='Palette'>
+      {/* Navigation */}
+      <div className ='Palette-colors'>
+        {/* Color Boxes */}
+      </div>
+      {/* Footer */}
+    </div>
+  );
+}
+
+export default App;
