331daf7c27f28896f68dc5540030c3caeaef9fc5
Mario <mariob360@gmail.com>
2019-09-23 14:12:05 -0700

    Fix Palette Route

--- src/components/App.js
+++ src/components/App.js
@@ -6,17 +6,28 @@ import { generatePalette } from '../utils/colorHelpers';
 
 
 class App extends Component {
+
+  // Return palette passed into routeProps
+  findPalette = (id) => (
+    seedColors.find(palette => palette.id === id)
+  )
+
   render() {
     return (
       <Switch>
         <Route exact path="/" render={() => <h1>Palette List Goes Here</h1> }/>
-        <Route exact path="/palette/:id"render={() => <Palette palette={generatePalette(seedColors[4])} />}/>
+        <Route
+        exact
+        path="/palette/:id"
+        render={(routeProps) => (
+          // PASS IN ROUTE PROPS
+          <Palette
+            palette={generatePalette(
+              this.findPalette(routeProps.match.params.id)
+              )}
+            />
+        )}/>
       </Switch>
-
-
-      // <div>
-      //   <Palette palette={generatePalette(seedColors[4])} />
-      // </div>
     );
   }
 }
