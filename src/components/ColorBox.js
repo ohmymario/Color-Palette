a8d289db8c7e467e0ff85980d45cd1270e427260
Mario <mariob360@gmail.com>
2019-10-01 23:10:50 -0700

    Finish refactor of ColorBox CSS

--- src/components/ColorBox.js
+++ src/components/ColorBox.js
@@ -56,6 +56,67 @@ const styles = {
   textDecoration: 'none',
   outline: 'none',
   opacity: '0',
+  },
+  boxContent: {
+    position: 'absolute',
+    padding: '10px',
+    width: '100%',
+    left: '0px',
+    bottom: '0px',
+    letterSpacing: '1px',
+    textTransform: 'uppercase',
+    fontSize: '12px',
+  },
+  copyOverlay: {
+    opacity: '0',
+    zIndex: '0',
+    width: '100%',
+    height: '100%',
+    transition: 'transform 0.6s ease-in-out',
+    transform: 'scale(0.1)',
+  },
+  showOverlay: {
+    position: 'absolute',
+    opacity: '1',
+    transform: 'scale(50)',
+    zIndex: '10',
+  },
+  copyMessage: {
+    position: 'fixed',
+    left: '0',
+    right: '0',
+    top: '0',
+    bottom: '0',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+
+    fontSize: '4rem',
+    transform: 'scale(0.1)',
+    opacity: '0',
+    color: 'white',
+    '& h1': {
+      fontWeight: '400',
+      textShadow: '1px 2px black',
+      background: 'rgba(255, 255, 255, 0.2)',
+      width: '100%',
+      textAlign: 'center',
+      marginBottom: '0',
+      padding: '1rem',
+      textTransform: 'uppercase',
+    },
+    '& p': {
+      fontSize: '2rem',
+      fontWeight: '100',
+    }
+  },
+  showMessage: {
+    opacity: '1',
+    transform: 'scale(1)',
+    zIndex: '25',
+    transition: 'all 0.4s ease-in-out',
+    transitionDelay: '0.1s',
   }
 }
 
@@ -82,20 +143,19 @@ class ColorBox extends Component {
       <CopyToClipboard text={ background } onCopy={this.changeCopyState}>
 
       {/* Individual Color Box */}
-      <div style={{background}} className={`${classes.ColorBox}`}>
+      <div style={{background}} className={classes.ColorBox}>
 
         {/* Growing Color Alert Box*/}
-        <div style={{background}} className={`copy-overlay ${copied && 'show'}`}/>
-        <div className={`copy-message ${copied && 'show'}`}>
+        <div style={{background}} className={`${classes.copyOverlay} ${copied && classes.showOverlay}`}/>
+        <div className={`${classes.copyMessage} ${copied && classes.showMessage}`}>
           <h1>Copied</h1>
           <p className={`${classes.textColor}`}>{background}</p>
         </div>
 
         {/* Box Contents */}
-        <div className='copy-container'>
-          <div className='box-content'>
-            {/* <span className={contrast ? 'white-text' : null}>{name}</span> */}
-            <span className={`${classes.textColor}`}>{name}</span>
+        <div>
+          <div className={classes.boxContent}>
+            <span className={classes.textColor}>{name}</span>
           </div>
           <button className={`${classes.copyButton} ${classes.textColor}`}>Copy</button>
         </div>
