b0ad585df0960d41014845096d42a5dc43f54973
Mario <mariob360@gmail.com>
2019-10-24 17:42:58 -0700

    Tweak styles of form

--- src/components/styles/NewPaletteFormStyles.js
+++ src/components/styles/NewPaletteFormStyles.js
@@ -6,7 +6,6 @@ const styles = theme => ({
   root: {
     display: 'flex',
   },
-
   hide: {
     display: 'none',
   },
@@ -22,6 +21,7 @@ const styles = theme => ({
   drawerHeader: {
     display: 'flex',
     alignItems: 'center',
+    width: '100%',
     padding: '0 8px',
     ...theme.mixins.toolbar,
     justifyContent: 'flex-end',
@@ -29,7 +29,7 @@ const styles = theme => ({
   content: {
     flexGrow: 1,
     height: 'calc(100vh - 64px)',
-    padding: theme.spacing(3),
+    padding: 0,
     transition: theme.transitions.create('margin', {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
